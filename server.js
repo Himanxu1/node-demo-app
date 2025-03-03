cf7d6765c15c49b9c453c4245de95830e56a81ba
himanxu2 <himanshu.singh@wherehouse.io>
2025-03-03 23:03:36 +0530

    test workflow

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import express from 'express'
 const app = express()
 
 app.get('/health-check', (req, res) => {
-  res.send({ message: 'app is healthy and tested' })
+  res.send({ message: 'app is healthy and tested and live' })
 })
 
 app.listen(3000, () => {
