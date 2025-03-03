ddb58b474ef1a0739892d70fbc8c02f965efd41a
himanxu2 <himanshu.singh@wherehouse.io>
2025-03-03 17:16:56 +0530

    test

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import express from 'express'
 const app = express()
 
 app.get('/health-check', (req, res) => {
-  res.send({ message: 'app is healthy' })
+  res.send({ message: 'app is healthy and tested' })
 })
 
 app.listen(3000, () => {
