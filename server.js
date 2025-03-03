477b76f5e084d4c666b36dfe784dc39554103fc9
himanxu2 <himanshu.singh@wherehouse.io>
2025-03-03 17:15:33 +0530

    added linting and formatting

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-import express from "express";
+import express from 'express'
 
-const app = express();
+const app = express()
 
-app.get("/health-check", (req, res) => {
-  res.send({ message: "app is healthy" });
-});
+app.get('/health-check', (req, res) => {
+  res.send({ message: 'app is healthy' })
+})
 
 app.listen(3000, () => {
-  console.log("app is running");
-});
+  console.log('app is running')
+})
